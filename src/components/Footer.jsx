796b30cf56522ef1ddc6ebfcc016e8f5a4864fec
james-fleming394 <james.fleming394@gmail.com>
2022-12-14 13:55:11 -0500

    Fix pictures

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import linkedin from '../assets/img/linkedin.png';
-import github from '../assets/img/github.png';
-import twitter from '../assets/img/twitter.png';  
+// import linkedin from '/public/img/linkedin.png';
+// import github from '/public/img/github.png';
+// import twitter from '/public/img/twitter.png';  
 
 
 const Footer = () => {
@@ -11,9 +11,9 @@ const Footer = () => {
                 <h4>Marvel Engine</h4>
                 <p>© 2022 Copyright: James Fleming</p>
                 <div className="social-icons">
-                    <a href="https://www.linkedin.com/in/james--fleming/"><img src={linkedin} alt="LinkedIn"></img></a>
-                    <a href="https://github.com/James-fleming394"><img src={github} alt="Github"></img></a>
-                    <a href="https://twitter.com/jflem394"><img src={twitter} alt="Twitter"></img></a>
+                    <a href="https://www.linkedin.com/in/james--fleming/"><img src='/img/linkedin.png' alt="LinkedIn"></img></a>
+                    <a href="https://github.com/James-fleming394"><img src='/img/github.png' alt="Github"></img></a>
+                    <a href="https://twitter.com/jflem394"><img src='/img/twitter.png' alt="Twitter"></img></a>
                 </div>
                 <div className="email">
                     <h5>Email: James.Fleming394@gmail.com</h5>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LOGO from '../assets/img/Marvel-Logo.png'
-import CART from '../assets/img/cart.png'
-import SEARCH from '../assets/img/search.png'
+// import LOGO from '../assets/img/Marvel-Logo.png'
+// import CART from '../assets/img/cart.png'
+// import SEARCH from '../assets/img/search.png'
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
@@ -12,11 +12,11 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar-logo" href="/">
-                <img className="marvel-logo" onClick={() => navigate('/home')} src={LOGO} alt="Marvel Logo" />
+                <img className="marvel-logo" onClick={() => navigate('/home')} src='/img/Marvel-Logo.png' alt="Marvel Logo" />
             <div className="search-bar">
                 <form>
                 <input className="search-bar-input" type="search" placeholder="Search Here..." />
-                <button className="search-button" type="submit"><img className="search-icon" src={SEARCH} alt="search"/></button>
+                <button className="search-button" type="submit"><img className="search-icon" src='/img/search.png' alt="search"/></button>
                 </form>
             </div>
             </div>
@@ -25,7 +25,7 @@ const Navbar = () => {
             <Link className="navbar-link" to="/register">Signup</Link>
             <Link className="navbar-link" to="/login">Login</Link>
             <Link className="navbar-link" to="/products">Products</Link>
-            <Link className="navbar-link" to="/cart"><img className="cart" src={CART} alt="cart-icon" /></Link>
+            <Link className="navbar-link" to="/cart"><img className="cart" src='/img/cart.png' alt="cart-icon" /></Link>
             </div>
         </div>
     )
